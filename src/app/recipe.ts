8a0d60c82e47d5c3687cca87f9b333591e0b3674
zickbalazs <zickbalazs@turr.hu>
2023-02-21 14:03:41 +0100

    Némely interface feltöltve

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -1,2 +1,16 @@
 export interface Recipe {
+    ID:number;
+    title:string;
+    description:string;
+    makingof:string;
+    image:string;
+    ingredients:Ingredient[];
+}
+export interface Ingredient{
+    ID:number;
+    foodID:number;
+    ingredientID:number;
+    name:string;
+    amount:number;
+    unit:string;
 }
